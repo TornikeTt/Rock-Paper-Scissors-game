7f00427164decb0b9bbcad3e27ad1515825ef7a1
Tornike <chargeishvilitornike@gmail.com>
2025-10-23 16:15:59 +0400

    Create layoutSetting function that determines which game type should run

--- src/Components/Game/Play.jsx
+++ src/Components/Game/Play.jsx
@@ -1,4 +1,5 @@
 import pentagonShape from "../../assets/images/bg-pentagon.svg";
+import triangleShape from "../../assets/images/bg-triangle.svg";
 
 import rock from "../../assets/images/icon-rock.svg";
 import paper from "../../assets/images/icon-paper.svg";
@@ -6,10 +7,28 @@ import scissors from "../../assets/images/icon-scissors.svg";
 import lizard from "../../assets/images/icon-lizard.svg";
 import spock from "../../assets/images/icon-spock.svg";
 
-function Play() {
-    return (
-        <div className="gamePlay">
-            <div className="pentagon-container">
+function Play({ gameMode }) {
+    const layoutSetting = () => {
+        if (gameMode === "rock-paper-scissors") {
+            return (
+                <div className="triangle">
+                    <img src={triangleShape} alt="triangle" />
+
+                    <button class="btn paper">
+                        <img src={paper} alt="paper" />
+                    </button>
+
+                    <button class="btn scissors">
+                        <img src={scissors} alt="scissors" />
+                    </button>
+
+                    <button class="btn rock">
+                        <img src={rock} alt="rock" />
+                    </button>
+                </div>
+            );
+        } else {
+            return (
                 <div className="pentagon">
                     <img src={pentagonShape} alt="pentagon" />
 
@@ -32,7 +51,13 @@ function Play() {
                         <img src={lizard} alt="lizard" />
                     </button>
                 </div>
-            </div>
+            );
+        }
+    };
+
+    return (
+        <div className="gamePlay">
+            <div className="shape-container">{layoutSetting()}</div>
         </div>
     );
 }
